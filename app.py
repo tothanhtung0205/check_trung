fdc46935879cbec50b8c8c81893654e9f23f4443
tothanhtung0205 <tothanhtung0205@gmail.com>
2018-08-22 10:54:58 +0700

    aaa

--- app.py
+++ app.py
@@ -16,18 +16,16 @@ def upload_files():
     if request.method == 'POST':
         t0 = time()
         f = request.files['file']
-        print(time()-t0)
-        print("Uploading...")
         f.save(secure_filename(f.filename))
         ff = f.filename
         print("Checking.....")
-        dupli_code,dupli_ques,dupli_ans = checking(ff)
-        os.remove(ff)
-
-        print("dupli code ")
-        print(dupli_code)
+        try:
+            dupli_code,dupli_ques,dupli_ans = checking(ff)
+        except:
+            return render_template('upload.html')
+        print("Checking time ")
+        print(time() - t0)
         return render_template('upload.html', dup_code = dupli_code , dup_qes = dupli_ques , dup_ans = dupli_ans )
 
-
 if __name__ == '__main__':
-    app.run(port=2222)
+    app.run( port=2222)
